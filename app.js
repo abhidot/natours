4f66e2ada516a3a937b62069a431cffa89b1c757
Abhishek Kumar Jain <theabhidot@gmail.com>
2021-05-18 00:31:41 +0530

    Basic express setup

--- app.js
+++ app.js
@@ -1,98 +1,30 @@
-const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+/// 1) MIDDLEWARES
+
 //Used to use middlewares in the requests
+// A middleware defined this way applies to all the routes declared after this.
+// All the middlewares are executed in the order they are declared
 app.use(express.json());
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+app.use(express.static(`${__dirname}/public`));
 
-const tours =  JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-
-app.get('/api/v1/tours', (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    result: tours.length,
-    data: {
-      tours,
-    },
-  });
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
 });
 
+//This is similar to creating a new sub-app for each of our resources.
+//This is known as Mounting of routers
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
-// :x is used to add a necessary parameter to the url
-// If you want this parameter to be optional then use :x?
-// Only last parameter can be optional
-app.get('/api/v1/tours/:id', (req, res) => {
-  
-  //Just a simple way in JS to convert a numeric string to a number
-  const id = req.params.id*1;
+/// 4) START SERVER
 
-  if(id > tours.length - 1){
-    return res.status(404).json({
-      status:'fail',
-      message:'Invalid ID'
-    })
-  }
-  const tour  = tours.find(el => el.id === id);
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
-});
-
-
-app.patch('/api/v1/tours/:id', (req, res) => {
-  const id = req.params.id*1;
-
-  if(id > tours.length - 1){
-    return res.status(404).json({
-      status:'fail',
-      message:'Invalid ID'
-    })
-  }
-
-  res.status(200).json({
-    status:'success',
-    tour:'<Updated tour here ...>'
-  })
-});
-
-
-app.delete('/api/v1/tours/:id', (req, res) => {
-  const id = req.params.id*1;
-
-  if(id > tours.length - 1){
-    return res.status(404).json({
-      status:'fail',
-      message:'Invalid ID'
-    })
-  }
-
-  res.status(204).json({
-    status:'success',
-    data:'null'
-  })
-});
-
-app.post('/api/v1/tours', (req, res) => {
-  const newId = tours.length;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
-    res.status(201).json({
-      status: 'success',
-      data: {
-        tour: newTour,
-      },
-    });
-  });
-});
-
-const port = 5000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+module.exports = app;
---
+++ controllers/tourController.js
@@ -0,0 +1,87 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable node/no-unsupported-features/es-syntax */
+const fs = require('fs');
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+exports.checkID = (req, res, next, val) => {
+  //Just a simple way in JS to convert a numeric string to a number
+  const id = req.params.id * 1;
+
+  if (id > tours.length - 1) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  next();
+};
+
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price',
+    });
+  }
+
+  next();
+};
+
+exports.getAllTours = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    result: tours.length,
+    requestedAt: req.requestTime,
+    data: {
+      tours,
+    },
+  });
+};
+
+exports.getTour = (req, res) => {
+  const id = req.params.id * 1;
+
+  const tour = tours.find((el) => el.id === id);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+
+exports.updateTour = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    tour: '<Updated tour here ...>',
+  });
+};
+
+exports.deleteTour = (req, res) => {
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
+
+exports.createTour = (req, res) => {
+  const newId = tours.length;
+  const newTour = { id: newId, ...req.body };
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/../dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
