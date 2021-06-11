9d8b494c2bfec80330aae83056cb29bc4f32a700
Abhishek Kumar Jain <theabhidot@gmail.com>
2021-06-11 15:59:50 +0530

    Webhook function

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -47,12 +47,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
-  console.log(session.customer_email);
   let user = await User.findOne({ email: session.customer_email });
-  console.log(user);
   user = user._id;
   const price = session.amount_total / 100;
-  console.log(tour, user, price);
   await Booking.create({ tour, user, price });
 };
 
